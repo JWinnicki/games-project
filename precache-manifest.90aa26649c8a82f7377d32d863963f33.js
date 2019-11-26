3ea521e9fc84683544236617376b1327e78c0c2a
Jedrzej Winnicki <jwinnicki2@gmail.com>
2019-11-26 18:46:34 +0100

    Updates

---
+++ precache-manifest.90aa26649c8a82f7377d32d863963f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f685e25afb6345f72ffa5851292feb33",
+    "url": "/Games-Project/index.html"
+  },
+  {
+    "revision": "b2060bd0ebe9742b7375",
+    "url": "/Games-Project/static/css/main.cb1ac43c.chunk.css"
+  },
+  {
+    "revision": "48230c09c5b6d4fe91c7",
+    "url": "/Games-Project/static/js/2.56ccc3ad.chunk.js"
+  },
+  {
+    "revision": "b2060bd0ebe9742b7375",
+    "url": "/Games-Project/static/js/main.ffd31223.chunk.js"
+  },
+  {
+    "revision": "61146db5071ff42a4f1d",
+    "url": "/Games-Project/static/js/runtime-main.08aaa11f.js"
+  },
+  {
+    "revision": "20d8a1a00ecb7c77f04fe007054fc86e",
+    "url": "/Games-Project/static/media/icons.20d8a1a0.svg"
+  }
+]);
